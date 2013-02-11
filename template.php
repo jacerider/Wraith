7fa6a0176ecd149139ee16a863aba57b93a4a33c
JaceRider <info@ashenrayne.com>
2013-02-11 09:05:06 -0600

    Include theme.

--- template.php
+++ template.php
@@ -3,6 +3,7 @@
 $path_to_wraith = drupal_get_path('theme', 'wraith');
 require_once $path_to_wraith . '/includes/assets.inc';     // CSS & JS alters
 require_once $path_to_wraith . '/includes/preprocess.inc'; // Preprocess hooks
+require_once $path_to_wraith . '/includes/theme.inc';      // Theme functions
 
 /**
  * Render SASS files.
